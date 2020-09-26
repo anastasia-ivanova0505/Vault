3495f13c267f5bfcb1b3e03838a8ca63607e645d
Ivanova Anastasia <anast.bogatireva2010@yandex.ru>
2020-09-26 13:27:58 +0300

    fix

---
+++ Network programming/HTTP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HttpWebRequest reqw = (HttpWebRequest)HttpWebRequest.Create("http://itstep.org");
+            //HttpWebResponse resp = (HttpWebResponse)reqw.GetResponse(); //создаем объект отклика
+
+            //using (StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.Default))
+            //{ //создаем поток для чтения отклика
+            //    Console.WriteLine(sr.ReadToEnd());
+            //    //вывести на экран все, что читается
+            //}
+
+
+            //foreach (string header in resp.Headers)
+            //    Console.WriteLine("{0}:{1}", header, resp.Headers[header]);
+
+
+            ////создание объекта web-клиент
+            //WebClient client = new WebClient();
+            ////получение содержимого странички
+            //byte[] urlData = client.DownloadData("http://itstep.org");
+            ////преобразование полученного содержимого в строку
+            ////для отображения в консоли
+            //string page = Encoding.ASCII.GetString(urlData);
+            //Console.WriteLine(page);
+
+
+            //создание объекта web-клиент
+            WebClient client = new WebClient();
+            string fileCopy = "E:\\ttt.jpg", urlString =
+             "https://www.yandex.ru/images/search?pos=1&nl=1&img_url=https%3A%2F%2Fsun9-65.userapi.com%2Fc854124%2Fv854124167%2F170b44%2F1bpvKBWDz8o.jpg&text=%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5%20%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B0%20%D0%BC%D0%B8%D1%80%D0%B0&rpt=simage";
+            //закачка web-ресурса в файл с именем fileCopy
+            client.DownloadFile(urlString, fileCopy);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Network programming/HttpRequest/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpRequest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(textBox1.Text);
+                req.Method = "Get";
+                if (checkBox1.Checked)
+                {
+                    WebProxy proxy = new WebProxy(textBox2.Text);
+                    proxy.Credentials = new NetworkCredential(textBox3.Text, textBox4.Text);
+                    req.Proxy = proxy;
+                }
+                HttpWebResponse rez = (HttpWebResponse)req.GetResponse();
+                StreamReader sr = new StreamReader(rez.GetResponseStream(), Encoding.Default);
+                textBox5.Text = sr.ReadToEnd();
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+    }
+}
